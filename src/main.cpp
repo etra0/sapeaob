0a123a40bd7d4d3c8cab96af4193037f59e67a63
Sebastián Aedo <sebastian.aedo@sansano.usm.cl>
2021-07-14 13:18:15 -0400

    Added scaffolding

---
+++ src/main.cpp
@@ -0,0 +1,5 @@
+#include <sapeaob/sapeaob.hpp>
+
+int main() {
+  std::cout << "Hello world!" << std::endl;
+}
---
+++ tests/test-aob.cpp
@@ -0,0 +1,5 @@
+#include <doctest/doctest.h>
+
+TEST_CASE("hello world") {
+  CHECK(true);
+}
