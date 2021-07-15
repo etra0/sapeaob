d415e8c18def20ce177134f1ba34b4cc8dd7ddad
Sebastián Aedo <sebastian.aedo@sansano.usm.cl>
2021-07-15 18:16:21 -0400

    First iteration of function generation
    Right now it supports arrays of type std::uint8_t. It doesn't has any
    boundary check.
    
    Includes some simple unit tests to check it's working correctly.

--- src/sapeaob/sapeaob.hpp
+++ src/sapeaob/sapeaob.hpp
@@ -1,1 +1,19 @@
 #include <iostream>
+
+template <std::uint8_t F, std::uint8_t... B>
+struct function_compare {
+  constexpr static bool compare(std::uint8_t* arr) {
+    return function_compare::compare_<F, B...>(arr, 0);
+  }
+
+  template <class _ =  void>
+  static constexpr bool compare_(std::uint8_t* arr, std::size_t offset) {
+    return true;
+  }
+
+  template <std::uint8_t F, std::uint8_t... B>
+  static constexpr bool compare_(std::uint8_t* arr, std::size_t offset) {
+    return *(arr + offset) == F && function_compare::compare_<B...>(arr, offset + 1);
+  }
+
+};
--- tests/test-aob.cpp
+++ tests/test-aob.cpp
@@ -1,5 +1,13 @@
 #include <doctest/doctest.h>
+#include <string>
+#include <vector>
+#include <sapeaob/sapeaob.hpp>
 
-TEST_CASE("hello world") {
-  CHECK(true);
+TEST_CASE("check first byte is correct") {
+  std::uint8_t test_arr[] = { 0xAA, 0xBB, 0xCC, 0xDD };
+  CHECK(function_compare<0xAA, 0xBB, 0xCC>::compare(test_arr));
+  CHECK(function_compare<0xBB, 0xCC, 0xDD>::compare(test_arr + 1));
+
+  // This one should fail.
+  CHECK(!function_compare<0xAA, 0xBB, 0xAA>::compare(test_arr));
 }
