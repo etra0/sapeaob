00d6554086c7c33ec20bac38b4d498b95aba356f
Sebastián Aedo <sebastian.aedo@sansano.usm.cl>
2021-08-04 11:10:57 -0400

    api: Add docstrings.
    Added some docstrings to make the usage of the library more easy.
    Added the `noexcept` to `find_all_matches` since this returns an empty
    vector instead of throwing an exception.

--- src/sapeaob/sapeaob.hpp
+++ src/sapeaob/sapeaob.hpp
@@ -17,6 +17,11 @@ namespace impl {
 // pattern. It doesn't has any size check so if you go out of bounds it will
 // most likely crash. Should be use for internal stuff mostly.
 template <std::uint16_t... Pattern> struct function_compare {
+  // This function calls compare_ which takes an index_sequence to avoid
+  // recursive solution using C++17 goodies. compare_ will call compare_one_ for
+  // every byte in the pattern. This specification was needed since you have
+  // special cases for certain bytes, specifically to the byte 0x100 which means
+  // ANY, that acts as a wildcard in the pattern.
   template <class it> constexpr static bool compare(it arr) {
     return function_compare::compare_<it>(
         arr, std::make_index_sequence<sizeof...(Pattern)>());
@@ -33,7 +38,8 @@ private:
   // We make sure we're not using any other value other than ANY that are bigger
   // than std::uint8_t
   template <class it, std::uint16_t Byte>
-  static constexpr std::uint8_t compare_one_(it arr, std::size_t offset) {
+  static inline constexpr std::uint8_t compare_one_(it arr,
+                                                    std::size_t offset) {
     constexpr const std::uint8_t val = Byte & 0xFF;
     // TODO: Find how to use a better constraint.
     static_assert(Byte < 0x101, "You can only use values of size 1");
@@ -42,6 +48,8 @@ private:
   }
 };
 
+// Wrapper around the BSF instruction for x86 assembly. This one uses the x64
+// bits variant.
 inline char bitscanfwd(unsigned long *n, unsigned long long y) {
 #ifdef _WIN32
   return _BitScanForward64(n, y);
@@ -55,6 +63,8 @@ inline char bitscanfwd(unsigned long *n, unsigned long long y) {
 #endif
 }
 
+// Finds the first 0 byte (byte, not bit) from LSB to MSB
+// in a little-endian machine. If no byte is found, it returns nullopt.
 static std::optional<unsigned long> find_index(std::uint64_t x) {
   using optional_long = std::optional<unsigned long>;
   const std::uint64_t special_value = 0x7F7F7F7F7F7F7F7F;
@@ -86,8 +96,15 @@ template <class it, std::uint16_t... Pattern> class step_calculator {
   }
 
 public:
-  explicit step_calculator() {};
+  explicit step_calculator(){};
 
+  // returns the maximum step possible by checking if the first byte of the
+  // pattern is found in a 8-byte lookup. If the first byte is not present in
+  // the next 8 bytes, it will return 8. If it is present, it'll return the
+  // offset in bytes between the current position and the position of the first
+  // byte of the pattern. For example if the first byte is 0xC0, and the lookup
+  // is 0xAABBC0DD_FFEE0011, this function will return 2 since 0xC0 is the third
+  // byte in that lookup.
   inline std::size_t get_step(it arr, std::size_t offset, std::size_t size) {
     if (offset + 8 <= size) {
       std::uint64_t casted_value = *(std::uint64_t *)&*(arr + offset);
@@ -105,13 +122,27 @@ public:
 template <std::uint16_t... Pattern> struct pattern {
   explicit pattern(){};
 
+  // Search for the pattern in the specific array. If there's a match, it will
+  // return the offset.
+  // In the case there's nothing found, this function throws the
+  // `sapeaob::pattern_not_found` exception.
   template <class it> std::size_t scan_match_offset(it arr, std::size_t size);
+
+  // Search for the pattern. If there's a match, returns the pointer of the
+  // match. If there's nothing found, this function throws the
+  // `sapeaob::pattern_not_found` exception.
   template <class it> std::uintptr_t scan_match(it arr, std::size_t size);
+
+  // Find all the matches in the array. If nothing is found, this function
+  // returns an empty vector.
   template <class it>
-  std::vector<std::uintptr_t> find_all_matches(it arr, std::size_t size);
+  std::vector<std::uintptr_t> find_all_matches(it arr,
+                                               std::size_t size) noexcept;
 
 private:
   template <class it>
+  // Implementation of the `scan_match_offset`. This one uses offset since if
+  // there's a match, we can still iterate from the first offset.
   std::size_t scan_match_offset(it arr, std::size_t size, std::size_t offset);
 };
 
@@ -150,7 +181,7 @@ std::uintptr_t pattern<Pattern...>::scan_match(it arr, std::size_t size) {
 template <std::uint16_t... Pattern>
 template <class it>
 std::vector<std::uintptr_t>
-pattern<Pattern...>::find_all_matches(it arr, std::size_t size) {
+pattern<Pattern...>::find_all_matches(it arr, std::size_t size) noexcept {
   std::vector<std::uintptr_t> matches{};
   std::size_t offset = 0;
   auto arr_ptr = reinterpret_cast<std::uintptr_t>(arr);
--- src/sapeaob/utils.hpp
+++ src/sapeaob/utils.hpp
@@ -1,8 +1,8 @@
 #include <cstdint>
 #include <emmintrin.h>
+#include <sapeaob/errors.hpp>
 #include <smmintrin.h>
 #include <utility>
-#include <sapeaob/errors.hpp>
 
 #ifndef SAPEAOB_UTILS
 #define SAPEAOB_UTILS
@@ -10,22 +10,35 @@
 namespace sapeaob {
 namespace utils {
 
+// Takes a byte and a sequence of the size of TargetType and
+// sprays that byte to the entire size of TargetType. For example
+// if TargetType is std::uint32_t, then sizeof(std::uint32_t) = 4,
+// and v = 0xC0, the result will be 0xC0C0C0C0.
+// This function shouldn't be used by itself since it already requires a
+// sequence as an input. You should use `merge_bytes` instead.
 template <typename TargetType, std::size_t... Sequence>
 constexpr TargetType spray_byte(std::uint8_t v,
-  std::index_sequence<Sequence...>) {
+                                std::index_sequence<Sequence...>) {
   TargetType result = 0;
   ((result |= static_cast<TargetType>(v) << (8 * Sequence)), ...);
   return result;
 }
 
+// Variant of merge_bytes which allows you to use a TargetType that's bigger
+// than one and spray that byte using the `spray_byte` function. Useful when you
+// have a big type and you want to cast a single byte to it.
 template <typename TargetType>
 constexpr TargetType merge_bytes(std::uint8_t item) {
   static_assert(sizeof(TargetType) > 1,
                 "Don't use this function for types with size 1.");
   int v = 0;
-  return spray_byte<TargetType>(item, std::make_index_sequence<sizeof(TargetType)>());
+  return spray_byte<TargetType>(item,
+                                std::make_index_sequence<sizeof(TargetType)>());
 };
 
+// This function takes a variadic number of bytes and a type and join the bytes
+// and creates a TargetType with those bytes joint. The number of bytes and the
+// size of TargetType should be the same.
 template <typename TargetType, class... Items>
 constexpr TargetType merge_bytes(Items... items) {
   int pos = 0;
